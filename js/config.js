6d153067c3ad12defc96a2893c39f71f4541eb9f
Josephx64 <josephx64@gmail.com>
2015-01-20 09:07:09 -0600

    Inclusión de archivos pendientes

---
+++ js/config.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+
+	/*** Método click del boton #btnImprimir ***/
+	function imprimir(){
+		window.print();
+	}
+
+	document.getElementById("btnImprimir").onclick = imprimir;
+
+	
+
+	/*** Popover de la nota alpie #1.2-1 ***/
+	$(function () {
+		$('[data-toggle="popover"]').popover()
+	})
+
+	$('this').popover('toggle');
+
+
+	/*** ***/
+	
+	
+	
+}
