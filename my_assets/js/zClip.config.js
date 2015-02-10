4f142403314b25952a1adf6dbcd1cb3560adce3d
Josephx64 <josephx64@gmail.com>
2015-02-09 18:10:22 -0600

    Copy-to-clipboard Update & jQuery v1.72 for Dokeos and v 1.11.1 for Plugins

--- my_assets/js/zClip.config.js
+++ my_assets/js/zClip.config.js
@@ -1,13 +1,24 @@
 // zClip.config.js
-var client = new ZeroClipboard( document.getElementById("copy-button") );
+var client = new ZeroClipboard( document.getElementById("btnCopiar") );
+
+/*client.on( "mouseover", function() ){
+    this.classList.add("btnCopiar-hover");
+};*/
 
 client.on( "ready", function( readyEvent ) {
-  // alert( "ZeroClipboard SWF is ready!" );
+    // alert( "ZeroClipboard SWF is ready!" );
 
-  client.on( "aftercopy", function( event ) {
-    // `this` === `client`
-    // `event.target` === the element that was clicked
-    //event.target.style.display = "none";
-    alert("Copied text to clipboard: " + event.data["text/plain"] );
-  } );
+    client.on( "mouseover", function() ){
+        this.classList.add("btnCopiar-hover");
+    };
+
+    client.on( "aftercopy", function( event ) {
+        // `this` === `client`
+        // `event.target` === the element that was clicked
+        //event.target.style.display = "none";
+        event.target.style.display = "html";
+        //alert("Texto copiado al portapapeles: " + event.data["text/html"] );
+        alert("Código copiado al portapapeles");
+      } );
+
 } );
