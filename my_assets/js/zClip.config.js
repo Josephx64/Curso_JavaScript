d40650c9871ed6212381daa11236ccd11d2c68d8
Josephx64 <josephx64@gmail.com>
2015-03-02 11:05:42 -0600

    Update 150302

--- my_assets/js/zClip.config.js
+++ my_assets/js/zClip.config.js
@@ -8,16 +8,12 @@ var client = new ZeroClipboard( document.getElementById("btnCopiar") );
 client.on( "ready", function( readyEvent ) {
     // alert( "ZeroClipboard SWF is ready!" );
 
-    client.on( "mouseover", function() ){
-        this.classList.add("btnCopiar-hover");
-    };
-
     client.on( "aftercopy", function( event ) {
         // `this` === `client`
         // `event.target` === the element that was clicked
         //event.target.style.display = "none";
         event.target.style.display = "html";
-        //alert("Texto copiado al portapapeles: " + event.data["text/html"] );
+        alert("Texto copiado al portapapeles: " + event.data["text/html"] );
         //alert("Código copiado al portapapeles");
       } );
 
